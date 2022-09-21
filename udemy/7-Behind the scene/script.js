01f85cedf36ad19420c7a494ea1dda4ab9aa5a4d
AaaayushXD <aayushlamichhane.hehe@gmail.com>
2022-09-21 22:37:22 +0545

    1st

--- udemy/7-Behind the scene/script.js
+++ udemy/7-Behind the scene/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 function calcAge(birthYear) {
     const age = 2022 - birthYear;
     console.log(age, firstName);
@@ -24,3 +24,57 @@ function calcAge(birthYear) {
 
 const firstName = 'Aayush';
 calcAge(1992);
+
+
+
+//Hoisting Example
+
+console.log(me);
+// console.log(job);
+// console.log(years);
+
+var me = 'Aayush';
+let job = 'Student';
+const years = 2004;
+
+console.log(add(2, 3));
+// console.log(addEx(2,3));
+// console.log(addArrow(2, 3));
+
+function add(a, b) {
+    return a + b;
+}   //this can only be used before initialization.
+
+const addEx = function (a, b) {
+    return a + b;
+}
+
+const addArrow = (a, b) => {
+    return a + b;
+}
+
+
+//Example
+
+let num = 10;
+if (!num) deleteCart();
+
+function deleteCart() {
+    console.log('All Products deleted');
+}*/
+
+
+console.log(this);
+
+const calcAge = function (birthYear) {
+    console.log(2022 - birthYear);
+    console.log(this);
+}
+calcAge(2004);
+
+
+const calcAgeArrow  = (birthYear) => {
+    console.log(2022 - birthYear);
+    console.log(this);
+}
+calcAgeArrow(2000);
