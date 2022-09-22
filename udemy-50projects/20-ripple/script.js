81b410f05bd9f7df6f74e2574708e7163e4aaf30
AaaayushXD <aayushlamichhane.hehe@gmail.com>
2022-09-22 16:25:08 +0545

    1

---
+++ udemy-50projects/20-ripple/script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const btn = document.querySelectorAll('.ripple');
+
+btn.forEach(button => {
+    button.addEventListener('click', function(e) {
+        const x = e.clientX;
+        const y = e.clientY;
+        
+        const top = e.target.offsetTop;
+        const left = e.target.offsetLeft;
+
+        const xInside = x - left;
+        const yInside = y - top;
+        
+        const circle = document.createElement('span');
+        circle.classList.add('circle');
+        circle.style.top = yInside + 'px';
+        circle.style.left = xInside + 'px';
+
+        this.appendChild(circle);
+
+        setTimeout(() => {
+            circle.remove()
+        }, 500);
+    })
+})
